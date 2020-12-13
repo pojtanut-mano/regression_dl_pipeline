400d7b4397dd6d9694445994b6614ac8435ca6c1
Pojtanut Manopnjasiri <poompasd@gmail.com>
2020-12-13 16:55:53 +0700

    Edit docstrings to be aligned with google docstring style

--- PytorchNetwork.py
+++ PytorchNetwork.py
@@ -4,7 +4,20 @@ from torch.optim.lr_scheduler import StepLR
 
 
 class Net(nn.Module):
-    def __init__(self, input_size, output_size):
+    """Create neural network placeholder
+
+    using Pytorch library to create neural network with the first layer (input layer) of specified size,
+    2 hidden layers with 4 and 8 hidden nodes respectively and output node of size 1 being scaled by ReLU
+    as a regression prediction result
+
+    Args:
+        input_size (int): dimension of input data
+        output_size (int): dimension of output (default=1)
+
+    Returns:
+        (torch.tensor): output torch tensor with attached computational graph
+    """
+    def __init__(self, input_size, output_size=1):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(input_size, 8)
         self.fc2 = nn.Linear(8, 4)
@@ -30,10 +43,37 @@ class Net(nn.Module):
 
 
 def within_15(y_true, y_pred):
+    """Return whether prediction is within 15 percent error of target
+
+    Return 1 if the absolute percentage of error of predicted target is within 15 percent of true label
+
+    Args:
+        y_true (torch.tensor): true labels
+        y_pred (torch.tensor): predicted labels
+
+    Returns:
+        None
+    """
     return int(torch.sum(torch.abs(y_true - y_pred) * 100. / y_true <= 15)) / len(y_true)
 
 
 def train(args, model, device, train_loader, optimizer, loss_fn, epoch):
+    """Process data into network and perform back propagation to train it
+
+    For each mini batch, run data through network and compute loss to adjust the weight of each neuron in network
+    the number of epoch is specified in args, and there is a choice to dry run the network
+
+    Args:
+        args: argument in argparse module
+        model (Net): Pytorch neural network that is already created by Net class as a sub-class of nn.Module
+        device: specify GPU used to train network if not specify will use CPU instead
+        train_loader: Pytorch DataLoader containing features and labels
+        optimizer: Specify Pytorch optimizer object to be used as network optimizer
+        loss_fn: Specify Pytorch loss function to calculate loss for each mini batch
+        epoch: number of epoch that will be used to train the model
+    Returns:
+        None
+    """
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -52,6 +92,18 @@ def train(args, model, device, train_loader, optimizer, loss_fn, epoch):
 
 
 def test(model, device, loss_fn, test_loader):
+    """Use test data to evaluate the performance of trained model
+
+      run data through model and compute loss, then report the accuracy
+
+       Args:
+           model (Net): Pytorch neural network that is already created by Net class as a sub-class of nn.Module
+           device: specify GPU used to train network if not specify will use CPU instead
+           loss_fn: Specify Pytorch loss function to calculate loss for each mini batch
+           test_loader: Pytorch DataLoader containing features and labels
+       Returns:
+           None
+       """
     model.eval()
     test_loss = 0
     correct = 0
